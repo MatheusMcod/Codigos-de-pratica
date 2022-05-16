294081365091694b43a9054eb14fe598f7877898
Matheus Moreira Costa <90786402+MatheusMcod@users.noreply.github.com>
2022-05-15 21:13:25 -0300

    Update

--- LandingPage5p/index.js
+++ LandingPage5p/index.js
@@ -6,6 +6,7 @@ const imgUp = document.getElementById('img-up');
 const imgDown = document.getElementById('img-down');
 const imgUp2 = document.getElementById('img-up2');
 const imgDown2 = document.getElementById('img-down2');
+const mobMenu = document.getElementById('menu-mob');
 
 function menuOpen() {
     open.classList.add('active');
@@ -31,6 +32,14 @@ function menuClose2() {
     imgDown2.classList.remove('remove');
 }
 
+function menuMob() {
+    const menu1 = document.getElementById('container-head1');
+    const menu2 = document.getElementById('container-head2');
+
+    menu1.classList.toggle('mob-active');
+    menu2.classList.toggle('mob-active');
+}
+
 
 trigger.addEventListener('mouseover', menuOpen);
 open.addEventListener('mouseover', menuOpen);
@@ -40,4 +49,5 @@ trigger2.addEventListener('mouseover', menuOpen2);
 open2.addEventListener('mouseover', menuOpen2);
 trigger2.addEventListener('mouseout', menuClose2);
 open2.addEventListener('mouseout', menuClose2);
+mobMenu.addEventListener('click', menuMob);
 
